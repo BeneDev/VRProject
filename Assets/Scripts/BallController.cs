1f82dd013883148ec7b7e50e2d5283a469fe1984
Benedikt Kirchmeier <33699887+BeneDev@users.noreply.github.com>
2018-09-05 11:46:23 +0200

    Balls get destroyed when under ground and ball spawner

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private void Awake()
+    {
+        InvokeRepeating("CheckYPosition", 1f, 1f);
+    }
+
+    void CheckYPosition()
+    {
+        if(transform.position.y <= -50f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+    int ballsInContainer = 0;
+    [SerializeField] int minBalls;
+    [SerializeField] int spawnCount = 3;
+    [SerializeField] Collider checkingCollider;
+    int ballLayer;
+    [SerializeField] GameObject ballPrefab;
+    [SerializeField] Transform spawnPoint;
+
+    private void Awake()
+    {
+        ballLayer = LayerMask.NameToLayer("Grabable");
+        InvokeRepeating("SpawnBalls", 0f, 1f);
+    }
+
+    void SpawnBalls()
+    {
+        if(ballsInContainer < minBalls)
+        {
+            for (int i = 0; i < spawnCount; i++)
+            {
+                Instantiate(ballPrefab, spawnPoint.position + new Vector3(Random.Range(-checkingCollider.bounds.extents.x, checkingCollider.bounds.extents.x), Random.Range(-checkingCollider.bounds.extents.y, checkingCollider.bounds.extents.y), 0f), Quaternion.Euler(Vector3.zero));
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.layer + "!=" + ballLayer);
+        if(other.gameObject.layer == ballLayer)
+        {
+            ballsInContainer++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.layer == ballLayer)
+        {
+            ballsInContainer--;
+        }
+    }
+
+}
